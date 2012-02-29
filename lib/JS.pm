3d569a9d29ba7ad10b492794c72213c2aa7a9195
Ingy döt Net <ingy@cpan.org>
2012-02-29 04:23:58 +0000

    Import of INGY/JS-0.18 from CPAN.
    gitpan-cpan-distribution: JS
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         INGY/JS-0.18.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/JS.pm
+++ lib/JS.pm
@@ -1,253 +1,197 @@
-# Copyright (C) 2010  Juan-Manuel Torres-Moreno
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# -----	Divergence de Jensesn-Shannon   	V 0.3     7-10 decembre 2009
-#  	Juan-Manuel Torres-Moreno LIA/Avignon   juan-manuel.torres@univ-avignon.fr
-package JS ;
-our $VERSION = '0.0130';
-use Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT = qw(&jensen_shannon);
+package JS;
+use strict;
+use warnings;
+use File::Find;
+use 5.008;
 
-use strict ;
-#------------------ Divergence de Jensen-Shannon : P = DISTRIBUTION_P ; P = P_w ; Q = DISTRIBUTION_Q ; Q = Q_w, w = symbols
-# 	JS 	= 0.5 * SUM_w P Log ( P /(P+Q)/2 ) + Q Log ( Q/(P+Q)/2 ) 
-# 	   	= 0.5 * SUM_w P Log ( 2P_w/(P_w+Q_w) ) + Q_w Log ( 2Q_w/(P_w+Q_w) )
-sub jensen_shannon {
-	my ($P, $Q) = @_;			# $P=pointeur dist P, $Q=pointeur dist Q  
-	my $log_2   = log(2) ;		# Logarithme base 2
-   	my %deja_vu = () ;			# Identifier les symboles de la distribution P
-   	map{ $deja_vu{$_}++ } (keys %$P) ;
-	my @symbols = sort keys %deja_vu ;	# Symbols de la distribution P
-     	my $js      = 0 ; 					# init Jensen-Shannon
-     	foreach my $mot (@symbols) { 		# Calcul de la divergence Jensen-Shannon
-		$js 	+= $P->{$mot} * log( 2*$P->{$mot} / ( $P->{$mot} + $Q->{$mot} ) )/$log_2 	# P Log ( P /(P+Q)/2 )
-			+  $Q->{$mot} * log( 2*$Q->{$mot} / ( $P->{$mot} + $Q->{$mot} ) )/$log_2 	# Q Log ( Q /(P+Q)/2 )
-     	}
-     	return $js/2 ;						# retourner divergence Jensen-Shannon
+our $VERSION = '0.18';
+
+sub new {
+    my $class = shift;
+    return bless {@_}, $class;
 }
-1;
 
-__END__
+sub run {
+    my $self = shift;
+    my @args = @_;
+
+    if (! @args) {
+        return $self->list_all();
+    }
+    for my $js_module (@args) {
+        $js_module =~ s/\.js$//;
+        my @path = $self->find_js_path($js_module)
+          or warn("*** Can't find $js_module\n"), next;
+        print join "\n", sort(@path), "";
+    }
+}
+
+sub list_all {
+    my $found = {};
+    find {
+        wanted => sub {
+            return unless -f $_;
+            return if /\.(?:pm|pod|packlist)$/;
+            return if /^\./;
+            my $dir = $File::Find::dir;
+            $dir =~ s{.*/JS\b(/|$)(.*)}{$2} or return;
+            my $module = $dir ? "$dir/$_" : $_;
+            if ($module =~ s/\.js$//) {
+                $module =~ s/[\/\\]+/./g;
+            }
+            return if $found->{$module}++;
+            print $module, "\n";
+        },
+    }, grep {-d $_ and $_ ne '.'} @INC;
+}
+
+sub find_js_path {
+    my $self = shift;
+    my $module = shift;
+    unless ($module =~ /\//) {
+        $module =~ s/(?:\.)/\//g;
+    }
+    $module =~ s/(?:::)/\//g;
+    $module =~ s/\*$/.*/;
+
+    my $found = {};
+    my @module_path;
+    find {
+        wanted => sub {
+            my $path = $File::Find::name;
+            while (1) {
+                return if -d $_;
+                return if $path =~ /[\\\/]$module\.pm$/i;
+                return if $path =~ /[\\\/]$module\.pod$/i;
+                last if $path =~ /[\\\/]$module$/i;
+                last if $path =~ /[\\\/]$module\.js(?:\.gz)?$/i;
+                return;
+            }
+            return if $found->{$path}++;
+            push @module_path, $path;
+        },
+    }, grep {-d $_ and $_ ne '.'} @INC;
+    return @module_path;
+}
+
+1;
 
 =head1 NAME
 
-JensenShannon - Compute the Jensen-Shannon divergence between two probability distributions 
+JS - JavaScript Modules on CPAN
 
-=head1 SYNOPSIS
+=head1 SYSNOPSIS
 
-use strict  ;
-use JS ;   						# Module Jensen_Shannon
-
-my $A = "la plume de ma tante est la plus jolie" ; 	# Doc 1
-my $B = "la plume est jolie" ; 					# Doc 2
-
-my %A = unigrammes(split(/ /,$A));			# 1-grammes
-my %B = unigrammes(split(/ /,$B)) ;
-my %P  = () ;  my %Q = () ;				# Distributions de probabilit�s de P(A) et Q(B)
-foreach my $mot (keys %A) {			
-	$P{$mot} = $A{$mot}/keys %A ; 						
-	if (exists $B{$mot}) 	{ $Q{$mot} = $B{$mot}/keys %B }
-					else { $Q{$mot} = $A{$mot}/keys %A  }  # Smooth simple
-}
-print "Divergence JS = ", JS::jensen_shannon(\%P, \%Q),"\n" ;		# Divergence de Jensen-Shannon	
-
-#---------------------- Calcul de 1-grammes
-sub unigrammes {					
-	my %unigrammes = () ;
-	for (my $i = 0; $i < @_; $i++) {		# Parcourir chaque mot de $text
-      		$unigrammes{$_[$i]}++;     		# Stocker le unigramme dans l'index du hachage %unigrammes
- 	}
- 	return %unigrammes 
-}
+    > # Typical unix command line stuff:
+    > sudo cpan JS::jQuery
+    ... cpan installs JS-jQuery ...
+    > js-cpan
+    jquery-1.2.3
+    jquery-1.2.3.min
+    jquery-1.2.3.pack
+    jQuery
+    > js-cpan jQuery*
+    /Library/Perl/5.8.8/JS/jQuery.js
+    /Library/Perl/5.8.8/JS/jquery-1.2.3.js
+    /Library/Perl/5.8.8/JS/jquery-1.2.3.min.js
+    /Library/Perl/5.8.8/JS/jquery-1.2.3.min.js.gz
+    /Library/Perl/5.8.8/JS/jquery-1.2.3.pack.js
+    > js-cpan jQuery.js
+    /Library/Perl/5.8.8/JS/jQuery.js
+    > cd my/webapp/that/requires/jquery/javascript/
+    > ln -s `js-cpan jQuery.js` jQuery.js
 
 =head1 DESCRIPTION
 
-This module computes Jensen-Shannon divergence between two 
-probabilities distribution.
+Some JavaScript modules can be installed from CPAN. This module comes
+with a utility called C<js-cpan> that helps you find JavaScript modules
+that have been installed on your system so that you can use them in
+various projects.
 
-Examples can be found in the distribution C<eg/> directory and the test
-file.
+=head1 EXPLANATION
 
-=head1 METHODS
+The JSAN project (L<http://openjsan.org>) has successfully provided much
+of the groundwork to make JavaScript module distributions look and act
+like Perl module distributions.
 
-=head2 x_data
+For example, the basic file layout is similar, the Test::Harness and
+Test::Simple framework has been ported to JSAN, and most modules use
+Makefiles to set things up.
 
-  $c->x_data( $y );
-  $x = $c->x_data;
+The Open JSAN project offers the tip off the iceberg in terms of
+being a CPAN for JavaScript. However it has a long way to go and not
+a lot of community to get it there. CPAN is a good place to put
+JavaScript modules.
 
-Return and set the one dimensional array reference data.  This is the
-"unit" array, used as a reference for size and iteration.
+Many projects require JavaScript components these days, and it would be
+nice to simply list them in the META.yml of your Perl project
+distributions.
 
-=head2 y_data
+There is a dead simple way to package non-Perl components into Perl/CPAN
+distributions. The components get installed in your Perl system but do
+not affect Perl in any other way.
 
-  $c->y_data( $y );
-  $x = $c->y_data;
+JS.pm is a module to explain and help maintain the JavaScript modules
+installed from CPAN.
 
-Return and set the one dimensional array reference data.  This vector
-is dependent on the x vector.
+Some module distributions will have both Perl and JavaScript components.
+Others will have only JavaScript components. All JavaScript modules and
+JavaScript-only distributions should have a top-level-namespace of 'JS'.
 
-=head2 size
+=head1 JS MODULE AUTHOR HOWTO
 
-  $c->size( $s );
-  $s = $c->size;
+It turns out that Perl's ExtUtils::MakeMaker will install *any*
+files that you put in the C<lib/> directory, into your C<perl>'s
+C<sitelib>. So setting up a JavaScript distribution is very similar to
+setting on a Perl one.
 
-Return and set the number of array elements.
+Say you have a JavaScript module called C<Foo.Bar>. First create a
+distribution directory called: C<JS-Foo-Bar>. Put your JavaScript code
+in C<lib/JS/Foo/Bar.js>. Put your documentation in
+C<lib/JS/Foo/Bar.pod>. Create a bare bones C<lib/JS/Foo/Bar.pm> Perl module so
+that CPAN related tools can find your stuff.
 
-=head2 x_rank
+Your Makefile.PL should look something like this:
 
-  $c->x_rank( $rx );
-  $rx = $c->x_rank;
+    use inc::Module::Install;
+    name     'JS-Foo-Bar';
+    abstract 'Sample JavaScript Module Distribution';
+    version  '0.01';
+    license  'lgpl';
+    all_from 'lib/JS/Foo/Bar.pod';
+    WriteAll;
 
-Return and set the ranks as an array reference.
+Create a C<Changes> and C<README> file and dummy C<test.t>. CPAN module
+distributions should have these files.
 
-=head2 y_rank
+Put your JavaScript tests in a directory called C<tests>. I'll write up
+more explicit instructions in a future release, but for now look at
+C<JS-YAML> on CPAN or any openjsan.org module as an example.
 
-  $ry = $c->y_rank;
-  $c->y_rank( $ry );
+Now just run these commands:
 
-Return and set the ranks as an array reference.
+    perl Makefile.PL
+    make
+    make manifest
+    make dist
+    cpan-upload -user foo -passwd bar -mailto foo@bar.com JS-Foo-Bar-0.01.tar.gz
 
-=head2 x_ties
+That's it. You've joined the revolution. :)
 
-  $xt = $c->x_ties;
-  $c->x_ties( $xt );
+NOTE: There is a working example JavaScript module shipped with C<JS.pm>
+      in the C<examples/JS-Foo-Bar> directory.
 
-Return and set the ties as a hash reference.
+=head1 AUTHOR
 
-=head2 y_ties
+Ingy döt Net <ingy@cpan.org>
 
-  $yt = $c->y_ties;
-  $c->y_ties( $yt );
+=head1 COPYRIGHT
 
-Return and set the ties as a hash reference.
+Copyright (c) 2008. Ingy döt Net.
 
-=head2 spearman
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
-  $n = $c->spearman;
-
-Spearman's rho rank-order correlation is a nonparametric measure of 
-association based on the rank of the data values and is a special 
-case of the Pearson product-moment correlation.
-
-      6 * sum( (xi - yi)^2 )
-  1 - --------------------------
-             n^3 - n
-
-Where C<x> and C<y> are the two rank vectors and C<i> is an index 
-from one to B<n> number of samples.
-
-=head2 kendall
-
-  $t = $c->kendall;
-
-         c - d
-  t = -------------
-      n (n - 1) / 2
-
-Where B<c> and B<c> are the number of concordant and discordant pairs
-and B<n> is the number of samples.  If there are tied pairs, a
-different (more complicated) denominator is used.
-
-=head2 csim
-
-  $n = $c->csim;
-
-Return the contour similarity index measure.  This is a single 
-dimensional measure of the similarity between two vectors.
-
-This returns a measure in the (inclusive) range C<[-1..1]> and is 
-computed using matrices of binary data representing "higher or lower" 
-values in the original vectors.
-
-This measure has been studied in musical contour analysis.
-
-=head1 FUNCTIONS
-
-=head2 rank
-
-  $v = [qw(1 3.2 2.1 3.2 3.2 4.3)];
-  $ranks = rank($v);
-  # [1, 4, 2, 4, 4, 6]
-  my( $ranks, $ties ) = rank($v);
-  # [1, 4, 2, 4, 4, 6], { 1=>[], 3.2=>[]}
-
-Return an list of an array reference of the ordinal ranks and a hash
-reference of the tied data.
-
-In the case of a tie in the data (identical values) the rank numbers
-are averaged.  An example will elucidate:
-
-  sorted data:    [ 1.0, 2.1, 3.2, 3.2, 3.2, 4.3 ]
-  ranks:          [ 1,   2,   3,   4,   5,   6   ]
-  tied ranks:     3, 4, and 5
-  tied average:   (3 + 4 + 5) / 3 == 4
-  averaged ranks: [ 1,   2,   4,   4,   4,   6   ]
-
-=head2 pad_vectors
-
-  ( $u, $v ) = pad_vectors( [ 1, 2, 3, 4 ], [ 9, 8 ] );
-  # [1, 2, 3, 4], [9, 8, 0, 0]
-
-Append zeros to either input vector for all values in the other that 
-do not have a corresponding value.  That is, "pad" the tail of the 
-shorter vector with zero values.
-
-=head2 co_sort
-
-  ( $u, $v ) = co_sort( $u, $v );
-
-Sort the vectors as two dimensional data-point pairs with B<u> values
-sorted first.
-
-=head2 correlation_matrix
-
-  $matrix = correlation_matrix( $u );
-
-Return the correlation matrix for a single vector.
-
-This function builds a square, binary matrix that represents "higher 
-or lower" value within the vector itself.
-
-=head2 sign
-
-Return 0, 1 or -1 given a number.
-
-=head1 TO DO
-
-Implement other rank correlation measures that are out there...
-
-=head1 SEE ALSO
-
-For the Jensen-Shannon divergence:
-
-L<http://en.wikipedia.org/wiki/Jensen%E2%80%93Shannon_divergence>
-
-=head1 THANK YOU
-
-Juan-Manuel Torres-Moreno<lt>juan-manuel.torres@univ-avignon.fr<gt>,
-
-=head1 AUTHOR AND COPYRIGHT
-
-Juan-Manuel Torres-Moreno <lt>juan-manuel.torres@cpan.org<gt>
-
-Copyright 2010, Juan-Manuel Torres-Moreno, All Rights Reserved.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute or modify it under
-the same terms as Perl itself.
+See L<http://www.perl.com/perl/misc/Artistic.html>
 
 =cut
